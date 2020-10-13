6d868caab58a20f5d58c292379806fb3b5643e78
Shewer Lu <shewer@gmail.com>
2020-10-13 18:54:30 +0800

    add complateion function  to  Tab key

--- lua/english/init.lua
+++ lua/english/init.lua
@@ -9,22 +9,18 @@
 --require "english/english_init"
 --string.find_word,string.word_info= require("english/english_dict")() 
 require('english.english_init')
+-- init  dictionary  function to string table 
 string.find_words,string.word_info= require('english.english_dict')() -- (USERDIR .. "\\" .. [[lua\english\english.txt]] )
 
-
+-- chcek mode   
 local function english_mode(env)
 	local context=env.engine.context
 	local ascii_mode= context:get_option("ascii_mode")
 	local _english_mode= context:get_option("english")
-	return  _english_mode   --and   not ascii_mode
+	return  _english_mode   and   not ascii_mode
 
 end 
-local function exchange(tag,seg,flag)
-	local segment=seg:back() 
-	segment._end= segment._end +1
-	segment.tags[tag]=true 
 
-end 
 local function lua_init()
 
 	local function processor_func(key,env) -- key:KeyEvent,env_
@@ -32,41 +28,41 @@ local function lua_init()
 		local context=env.engine.context 
 		local composition=context.composition
 		local keycode=key.keycode 
-		--if true then return k.Noop end 
 
+		
 		if (key:ctrl() or key:alt() or key:release() ) then return k.Noop end 
 
 		if not  english_mode(env)  then return k.Noop end  
 		--log.info("---processor in  normorl )" ) 
 		--log.info("---processor in inclish_mode   )" ) 
 
+		-- complate context.text  -- 
+		if  key:repr() == "Tab" then 
+			local seg=composition:back()
+			local cand=seg:get_candidate_at( seg.selected_index +1 ) 
+			if cand  then  context.input= cand.text end  
+			return k.Accepted 
+		end 
+
 		local keychar= (keycode >=0x20 and keycode <0x80 and string.char(keycode) ) or ""
-		if context:is_composing() and keychar == " " then
+		-- non_ascii code  
+		-- commit processor   "[, ]"
+		if context:is_composing() and keychar:match([[^[, ]$]])  then
+
 			local cand= context:get_selected_candidate()
-			if cand  then context.input= cand.text  end 
-			context:push_input(" ") 
+			context.input = (cand and cand.text ) or context.input
+			context:push_input(keychar) 
 			context:commit() 
 			context:clear()
 			return k.Accepted
 		end 
-		if not  keychar:match([[^[a-zA-Z._?*-]$]]) then  return k.Noop end 
-
 		log.info("---processor in inclish_mode:" .. context.input ..    "  ascii(" .. keychar .. ")" ) 
+		-- ascii   a-z A-Z_?*.-  
+		if not  keychar:match([[^[a-zA-Z_?*.-]$]]) then  return k.Noop end 
 		context:push_input(keychar)
-		--context:confirm_previous_selection()
-
-		--context.input= context.input .. keychar
-		--context:refresh_non_confirmed_composition()
-
 		log.info("---processor after in inclish_mode:" .. context.input ..    "  ascii(" .. keychar .. ")" ) 
-		--if keychar:match([[^[ ,%d]$]]) then
-		--return k.Noop
-		--end 
 		return k.Accepted 
 	end  
-	local function _processor_func(key,env)
-		return 2
-	end 
 
 	local function processor_init_func(env)
 	end 
@@ -76,27 +72,24 @@ local function lua_init()
 
 	-- lua segmentor
 	local function segmentor_func(segs ,env) -- segmetation:Segmentation,env_
-		if english_mode(env) then 
-			local context=env.engine.context
-			if context:is_composing() then 
-
-				log.info("--segment  context_input:" .. context.input .. "  segs_input:" .. segs.input .. " preedit:" .. context:get_preedit().text )
-				if segs.empty() then 
-					local seg=Segment(0,segs.input:len())
-					seg.tags=  Set({'english'})
-					segs:add_segment(seg) 
-				else 
-					local seg=segs:back()
-					seg:reopen(input:len()) 
-					seg.tags=  Set({'english'})
-
-				end 
-				return false
+		local context=env.engine.context
+		if english_mode(env) and context:is_composing() then 
+			log.info("--segment  context_input:" .. context.input .. "  segs_input:" .. segs.input .. " preedit:" .. context:get_preedit().text )
+			if segs.empty() then 
+				local seg=Segment(0,segs.input:len())
+				seg.tags=  Set({'english'})
+				seg.prompt="(english)"
+				segs:add_segment(seg) 
+				
+			else 
+				local seg=segs:back()
+				seg:reopen(input:len()) 
+				seg.prompt="(english)"
+				seg.tags=  Set({'english'})
 
 			end 
 			return false 
 		end 
-
 		return true
 	end 
 
@@ -106,32 +99,22 @@ local function lua_init()
 	end 
 	-- lua translator 
 	local function translator_func(input,seg,env)  -- input:string, seg:Segment, env_
-		if english_mode(env) then 
+		local context=env.engine.context
+		
+		if english_mode(env) and seg:has_tag("english")  then 
 			log.info("-----entry lua_tranlator : english   input:(" .. input ..")" )
-			local tags_= setmetatable(seg.tags,{__index=table })
-			local tt=""
-			for k,v in pairs(seg.tags) do 
-				tt = tt ..  (v and tt .."|" .. k)  or  ""
-			end 	
-			log.info("-----entry lua_tranlator : english   input:(" .. input ..") tags=" .. tt )
-
-			local type_= (seg:has_tag("english") and "english*" ) or  tt  
-			yield( Candidate(type_ , seg.start, seg._end, input,type_ ) )  -- debug  and first 
-
-			local context=env.engine.context
-			if  true then  --seg:has_tag("abc")   then 
-				--if  context:get_option("english")   then 
-				local str= input:sub(seg.start,seg.start + seg.length) 
-
-				str:find_words( 
-				function(elm) 
-					--log.info("--- in tran yield loop -elm:" .. elm )
-					local cand =Candidate("english", seg.start,seg._end, elm, "[english]")
-					yield( cand )
+			local flag=true 
+			context.input:find_words( 
+			function(elm) 
+				log.info("--- in tran yield loop -elm:" .. elm )
+				if flag and context.input ~= elm then 
+					yield( Candidate("pre_english", seg.start,seg._end , context.input , "[english]"))
 				end 
-				)
-
+				flag=false 
+				yield( Candidate("english", seg.start,seg._end, elm, "[english]") )
 			end 
+			)
+
 			log.info("-----exit lua_tranlato : english" )
 		end 
 	end 
