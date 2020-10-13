8b585bc249ab0508503208e9606ad0c4da7f93a9
Shewer Lu <shewer@gmail.com>
2020-10-13 20:12:45 +0800

    clear master file

--- rime.lua
+++
@@ -1,6 +0,0 @@
-local english = require("english")()
-english_processor = english.processor
-english_segmentor = english.segmentor
-english_translator = english.translator
-english_filter = english.filter
-english_filter0 = english.filter0
