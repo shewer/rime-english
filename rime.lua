6bb45260a000dcb5d0209c2152bf017e9020b52d
shewer <shewer@gmail.com>
2021-01-20 02:13:35 +0800

    remove file
    Signed-off-by: shewer <shewer@gmail.com>

--- rime.lua
+++
@@ -1,9 +0,0 @@
-
-USERDIR= ( USERDIR or  os.getenv("APPDATA") or "" ) .. [[\Rime]]
-
-require("english")("english") --  tag name  
---local english = require("english")()
---english_processor = english.processor
---english_segmentor = english.segmentor
---english_translator = english.translator
---english_filter = english.filter
